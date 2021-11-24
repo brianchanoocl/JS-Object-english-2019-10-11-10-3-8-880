d1b55a9c95817a5cd1b69bbcd7fa4e2ee26ba8f0
brianchanoocl <94883862+brianchanoocl@users.noreply.github.com>
2021-11-24 22:56:29 +0800

    answer

---
+++ main.js
@@ -0,0 +1,21 @@
+//  Q1
+
+var user = {};
+user.name = "John";
+user.surename = "Mike";
+user.name = "Peter";
+delete user.name;
+console.log(user);
+
+//////////////////////////////////////////////////////////////////
+
+//  Q2
+
+var fruit = {
+apple: 20,
+pear: 20,
+peach: 10
+};
+// write your code here...
+var sum = fruit.apple + fruit.pear + fruit.peach;
+console.log(sum);
